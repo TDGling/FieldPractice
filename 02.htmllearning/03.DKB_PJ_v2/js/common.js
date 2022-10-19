6478198ff92402c35066949d829b51e360acc0f2
TDGling <zime7543@gmail.com>
2022-10-19 16:51:15 +0900

    MyNew

--- 02.htmllearning/03.DKB_PJ_v2/js/common.js
+++ 02.htmllearning/03.DKB_PJ_v2/js/common.js
@@ -1,12 +1,14 @@
 // 도깨비 PJ v2 - 공통기능 JS - common.js
 
-// 로드구역
+////////////////// 로드구역 ////////////////
 window.addEventListener("DOMContentLoaded", () => {
-    console.log("로딩");
-    // 햄버거 버튼 클릭시 .top .on 추가 제거
-    // 전체미뉴 보이기 디자인 구현이 되어있음
-    // 대상 : ham
+    console.log("로딩완료!");
+
+    // 햄버거버튼 클릭시 .top에 .on추가/제거
+    // 전체메뉴 보이기 디자인 구현이 이미되어있음!
+    // 대상: .ham, .top
     const ham = document.querySelector(".ham");
     const top = document.querySelector(".top");
+
     ham.onclick = () => top.classList.toggle("on");
-});
+}); ///////////// 로드구역 /////////////////
