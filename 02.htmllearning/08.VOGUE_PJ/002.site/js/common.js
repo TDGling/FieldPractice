640b93800580174ce7bfe734f6afa3744dd5bbe0
TDGling <zime7543@gmail.com>
2022-11-01 13:05:21 +0900

    MyNew

--- 02.htmllearning/08.VOGUE_PJ/002.site/js/common.js
+++ 02.htmllearning/08.VOGUE_PJ/002.site/js/common.js
@@ -13,12 +13,15 @@ $(() => {
     // 이벤트 대상: window
     // 변경대상: #top
     const topA = $("#top");
-    // 스크롤위치변수
+    // 변경대상: .tbtn
     const tbtn = $(".tbtn");
+    // 스크롤위치변수
     let scTop;
     // 마지막 스크룰위치값
     let lastSc = 0;
 
+    ///////////////////////////////////////
+    //////// 스크롤 이벤트 함수 /////////////
     $(window).scroll(() => {
         // 스크롤 위치값(this는 window)
         scTop = $(this).scrollTop();
@@ -33,19 +36,21 @@ $(() => {
             topA.addClass("on");
             // addClass(클래스명) - 클래스넣기
 
-            // 스크롤 방향에 따라 .up추가/제거
-            if (scTop > lastSc) { // 숨기기
-                // #top의 높이값 (동적으로 높이값 설정)
+            // 스크롤 방향에 따라 숨겼다보이는 top값 변경
+            if (scTop > lastSc) {
+                // 숨기기
+                // #top의 높이값(동적으로 높이값 설정!)
                 let temp = topA.innerHeight();
                 // 스크롤 아랫방향
-                topA.removeClass("up").css({top:-temp+"px"});
-                console.log(topA.innerHeight());
-                // height() - 피딩이 빠진 순수높이값
+                topA.css({ top: -temp + "px" });
+                // console.log(temp);
+                // height() - 패딩이 빠진 순수높이값
                 // innerHeight() - 패딩포함 내부높이값
             } ///// if ////
-            else { // 보이기
+            else {
+                // 보이기
                 // 스크롤 윗방향
-                topA.addClass("up");
+                topA.css({ top: "0" });
             } //// else /////
         } ///////// if /////////
         else {
@@ -67,25 +72,34 @@ $(() => {
         lastSc = scTop;
         ////////////////////////////
 
-        // 2. top 버튼 보이기 숨기기
-        if(scTop >= 300){
-            tbtn.addClass("on")
-        }
-        else{
-            tbtn.removeClass("on")
-        }
+        // 2. TOP버튼 보이기/숨기기
+        if (scTop >= 300) {
+            // 300이상
+            tbtn.addClass("on");
+        } ///// if /////
+        else {
+            // 300미만
+            tbtn.removeClass("on");
+        } ///// else /////
     }); //////// scroll /////////////////
-    tbtn.click(()=>{
+
+    ////// TOP버튼 클릭 설정 ///////
+    tbtn.click(() => {
         // 스크롤 최상단으로
         // 애니메이션 스크롤 이동
-        // 전체 스크롤 이동의 대상은
-        // > Html, body 두 최상위 요소를 대상으로 한다
-        // 그래야 모든 브라우저에서 공통으로 작동함
-        $("html,body").animate({
-            scrollTop:"0",
-        },800,"easeOutBounce")
+        // 전체스크롤 이동의 대상은?
+        // -> html,body 두 최상위 요소를
+        // 대상으로 한다! 왜? 그래야 모든
+        // 브라우저에서 공통으로 작동함!
+        $("html,body").animate(
+            {
+                scrollTop: "0",
+            },
+            800,
+            "easeOutQuart"
+        );
         // scrollTop 속성은 제이쿼리 전용
-        // 세로스크롤 위치값을 셋팅할 수 있다.
-        // 참고) 가로스크롤은 scrollLeft 임
-    })
+        // 세로스크롤 위치값을 셋팅할 수 있다!
+        // 참고) 가로스크롤은 scrollLeft 임!
+    }); /////// click ///////////
 }); //////////////// jQB ///////////////////
